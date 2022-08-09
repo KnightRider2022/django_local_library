3d9cc8fc2537391b7929b92087477fbee21dc571
KnightRider2022 <109430548+KnightRider2022@users.noreply.github.com>
2022-08-08 23:52:12 -0700

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -148,3 +148,7 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
